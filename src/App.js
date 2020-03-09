2e57f6ebd4400476ebc32ffebd0278d847d5a538
Priyansh <priyansh7299@gmail.com>
2020-03-09 16:00:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>This app is under development and should be up by 2020-03-10 23:59</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
